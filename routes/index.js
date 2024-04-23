12987532e966afda0f373671bab26585c6a69f28
alex <shizofranky@gmail.com>
2024-04-23 12:00:46 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,22 @@ const { mw } = require('../middleware');
 
 const router = require('express').Router() ;
 
+const port = process.env.PORT || 3001 ;
+
+router.get('/home' , mw , async (req , res) => {
+    res.status(200).json({
+        message:`hello from my server , on ${port}` ,
+        port , 
+    });
+});
+
 router.get('/' ,
     mw ,
     async (req , res) => {
-        res.status(200).json(`hello from my server , on ${process.env.PORT}`);
+        res.status(200).json({
+            message:`hello from my server , on ${port}` ,
+            port , 
+        });
     }
 )
 
